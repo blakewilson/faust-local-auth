467210742d6561af9dea463db500bd2342f20015
Blake Wilson <blake@blake.id>
2021-11-23 10:54:23 -0600

    Log process

--- src/pages/api/faust/[[...route]].ts
+++ src/pages/api/faust/[[...route]].ts
@@ -1,4 +1,6 @@
-import 'faust.config';
-import { apiRouter } from '@faustjs/core/api';
+import "faust.config";
+import { apiRouter } from "@faustjs/core/api";
+
+console.log(process.env);
 
 export default apiRouter;
